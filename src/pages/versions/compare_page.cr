4e21c1fce72d70ba865fc6f923628936e56e0e6a
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-19 19:42:46 -0400

    Add in form for version comparison

--- src/pages/versions/compare_page.cr
+++ src/pages/versions/compare_page.cr
@@ -4,14 +4,26 @@ class Versions::ComparePage < MainLayout
   needs to : String
 
   def content
-    div class: "pb-4 space-x-4 text-lg text-center" do
-      span from
+    form(method: "GET", class: "pb-4 space-y-2 text-lg text-center", action: Home::Index.path) do
+      select_tag name: "from", class: "border rounded pr-2" do
+        Version::SUPPORTED_VERSIONS.each do |version|
+          option version, attrs: [version == from ? :selected : nil].compact
+        end
+      end
 
-      span do
+      span class: "px-4" do
         raw("&#8594;")
       end
 
-      span to
+      select_tag name: "to", class: "border rounded pr-2" do
+        Version::SUPPORTED_VERSIONS.each do |version|
+          option version, attrs: [version == to ? :selected : nil].compact
+        end
+      end
+
+      div do
+        submit("Compare", class: "w-40 border rounded cursor-pointer")
+      end
     end
 
     div "Loading diff...", data_controller: "diff-to-html", data_diff_to_html_unified_diff: diff
