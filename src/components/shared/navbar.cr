2560178a0a6e4a96348e84d0fd32e66ab735cf72
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-19 19:57:30 -0400

    Add GitHub logo

--- src/components/shared/navbar.cr
+++ src/components/shared/navbar.cr
@@ -1,7 +1,10 @@
 class Shared::Navbar < BaseComponent
   def render
-    nav class: "w-full bg-gray-200 px-6 py-4" do
-      link "LuckyDiff", to: Home::Index, class: "font-semibold text-lg"
+    nav class: "w-full flex justify-between bg-gray-200 px-6 py-4" do
+      link "LuckyDiff", to: Home::Index, class: "my-auto font-semibold text-lg"
+      a href: "https://github.com/stephendolan/lucky_diff", target: "_blank", class: "h-8 w-8" do
+        img src: asset("images/github.svg")
+      end
     end
   end
 end
