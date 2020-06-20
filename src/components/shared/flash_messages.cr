158c5c101fd23b663175c193e6355669639b7023
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-20 13:59:12 -0400

    Use flash pill to communicate flash message type

--- src/components/shared/flash_messages.cr
+++ src/components/shared/flash_messages.cr
@@ -7,7 +7,7 @@ class Shared::FlashMessages < BaseComponent
     div class: "bg-blue-900 text-center py-4 lg:px-4" do
       flash.each do |flash_type, flash_message|
         div class: "p-2 bg-blue-800 items-center text-blue-100 leading-none lg:rounded-full flex lg:inline-flex", role: "alert" do
-          span "Oops!", class: "flex rounded-full bg-indigo-500 px-2 py-1 text-xs font-bold mr-3"
+          span flash_type, class: "flex rounded-full bg-indigo-500 px-2 py-1 uppercase text-xs font-bold mr-3"
           span flow_id: "flash", class: "font-semibold mr-2 text-left flex-auto" do
             text flash_message
           end
