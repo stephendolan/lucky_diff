e5b37465d948ea115ee7a06736750a65350a6da3
Stephen Dolan <dolan.stephen1@gmail.com>
2020-06-27 10:48:05 -0400

    Add Kindmetrics view tracking

--- src/components/shared/layout_head.cr
+++ src/components/shared/layout_head.cr
@@ -9,6 +9,14 @@ class Shared::LayoutHead < BaseComponent
       title "#{@page_title}"
       css_link asset("css/app.css"), data_turbolinks_track: "reload"
       js_link asset("js/app.js"), defer: "true", data_turbolinks_track: "reload"
+
+      # Set up page view tracking in production only
+      if Lucky::Env.production?
+        js_link src: "https://kindmetrics.io/js/track.js",
+          defer: true,
+          data_domain: "luckydiff.com"
+      end
+
       meta name: "turbolinks-cache-control", content: "no-cache"
       csrf_meta_tags
       responsive_meta_tag
