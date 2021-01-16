e59bc4e0521d83831a205263af9c10f43089854b
Stephen Dolan <dolan.stephen1@gmail.com>
2021-01-16 08:17:39 -0500

    Apply fixes

--- config/email.cr
+++ config/email.cr
@@ -1,18 +1,9 @@
 BaseEmail.configure do |settings|
   if Lucky::Env.production?
-    settings.adapter = Carbon::DevAdapter.new(print_emails: true)
+    settings.adapter = Carbon::DevAdapter.new
   elsif Lucky::Env.development?
     settings.adapter = Carbon::DevAdapter.new(print_emails: true)
   else
     settings.adapter = Carbon::DevAdapter.new
   end
-end
-
-private def send_grid_key_from_env
-  ENV["SEND_GRID_KEY"]? || raise_missing_key_message
-end
-
-private def raise_missing_key_message
-  puts "Missing SEND_GRID_KEY. Set the SEND_GRID_KEY env variable to 'unused' if not sending emails, or set the SEND_GRID_KEY ENV var.".colorize.red
-  exit(1)
 end
