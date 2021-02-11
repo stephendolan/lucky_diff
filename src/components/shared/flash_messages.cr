cc1dfee7bc361ed8583ec45fbd28a206385a4d0c
Stephen Dolan <dolan.stephen1@gmail.com>
2021-02-11 11:58:21 -0500

    Restyle flash messages

--- src/components/shared/flash_messages.cr
+++ src/components/shared/flash_messages.cr
@@ -4,10 +4,10 @@ class Shared::FlashMessages < BaseComponent
   def render
     return if flash.empty?
 
-    div class: "bg-blue-900 text-center py-4 lg:px-4" do
+    div class: "bg-green-900 text-center py-4 lg:px-4" do
       flash.each do |flash_type, flash_message|
-        div class: "p-2 bg-blue-800 items-center text-blue-100 leading-none lg:rounded-full flex lg:inline-flex", role: "alert" do
-          span flash_type, class: "flex rounded-full bg-indigo-500 px-2 py-1 uppercase text-xs font-bold mr-3"
+        div class: "p-2 bg-green-800 items-center text-green-100 leading-none lg:rounded-full flex lg:inline-flex", role: "alert" do
+          span flash_type, class: "flex rounded-full bg-green-500 px-2 py-1 uppercase text-xs font-bold mr-3"
           span flow_id: "flash", class: "font-semibold mr-2 text-left flex-auto" do
             text flash_message
           end
