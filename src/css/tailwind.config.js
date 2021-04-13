684206ef67e356cab995470c287e96141e686630
Stephen Dolan <dolan.stephen1@gmail.com>
2021-04-13 18:05:38 -0400

    Use tailwind JIT compiler

--- src/css/tailwind.config.js
+++ src/css/tailwind.config.js
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
 module.exports = {
+  mode: "jit",
   purge: [
     "./src/css/**/*.css",
     "./src/pages/**/*.cr",
