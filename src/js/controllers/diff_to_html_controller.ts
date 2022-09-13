9e024022c47912109344f857f4c176bb00795cc4
Stephen Dolan <dolan.stephen1@gmail.com>
2022-09-13 11:10:06 -0400

    Add back CSS assets for controller.

--- src/js/controllers/diff_to_html_controller.ts
+++ src/js/controllers/diff_to_html_controller.ts
@@ -5,6 +5,9 @@ import {
   Diff2HtmlUIConfig,
 } from "diff2html/lib/ui/js/diff2html-ui-slim.js";
 
+import "highlight.js/styles/github.css";
+import "diff2html/bundles/css/diff2html.min.css";
+
 export default class extends Controller {
   connect(): void {
     const diff2htmlUi = new Diff2HtmlUI(
